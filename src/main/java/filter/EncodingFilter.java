a14b611aaed8d696b492d7d5fbe6ec1e39d217ba
ValentinP1 <valikpronovic@gmail.com>
2026-06-27 15:15:39 +0200

    feat: implement encoding filter relization

---
+++ src/main/java/filter/EncodingFilter.java
@@ -0,0 +1,25 @@
+package filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.annotation.WebFilter;
+import jakarta.servlet.Filter;
+
+import java.io.IOException;
+
+
+@WebFilter(value = {
+        "/currencies", "/currency/*", "/exchangeRates", "/exchangeRate/*", "/exchange"
+})
+public class EncodingFilter implements Filter {
+
+  @Override
+  public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, SecurityException, ServletException {
+
+      servletResponse.setContentType("application/json");
+      servletResponse.setCharacterEncoding("UTF-8");
+      servletRequest.setCharacterEncoding("UTF-8");
+
+      filterChain.doFilter(servletRequest, servletResponse);
+  }
+
+}
