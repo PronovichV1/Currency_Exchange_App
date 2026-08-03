ee119d85e9e0b122b08eeb37c77fe3d242ca33a7
ValentinP1 <valikpronovic@gmail.com>
2026-08-03 12:53:21 +0200

    refactor: improoved ConnectionManager class

--- src/main/java/com/currency/exchange/util/ConnectionManager.java
+++ src/main/java/com/currency/exchange/util/ConnectionManager.java
@@ -1,18 +1,23 @@
 package com.currency.exchange.util;
 
+import java.io.*;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 
 public class ConnectionManager {
     public static String DB_URL = "jdbc:sqlite:";
-    public static String dbPath = System.getenv("DB_PATH");
+    public static String dbPath;
+    private static String PROPERTY_NAME = "db.properties";
+    private static String RESOURCES_KEY = "db.path";
+
+
 
     static {
         try {
-            if(dbPath == null){
-                dbPath = "E:/Projects/Currency_Exchange_App/src/main/resources/currency_exchange.db";
-            }
+            dbPath = loadPathFromProperties();
             dbPath = DB_URL + dbPath;
             Class.forName("org.sqlite.JDBC");
         } catch (ClassNotFoundException e) {
@@ -24,4 +29,34 @@ public class ConnectionManager {
         return DriverManager.getConnection(dbPath);
     }
 
-}
+
+    public static String loadPathFromProperties() {
+        Properties properties = new Properties();
+        String result;
+
+        /// read db.properties
+        try (InputStream input = ConnectionManager.class.getClassLoader().getResourceAsStream(PROPERTY_NAME)) {
+            if (input == null){
+                throw new RuntimeException("File " + PROPERTY_NAME + " doesn't found in resources!");
+            }
+            properties.load(input);
+            result = properties.getProperty(RESOURCES_KEY);
+
+            if (result != null && result.equals("${DB_PATH}")) {
+                Properties envProps = new Properties();
+                /// read .env
+                try (FileInputStream inputStream = new FileInputStream(".env")){
+                    envProps.load(inputStream);
+                    result = envProps.getProperty("DB_PATH");
+                } catch (IOException e){
+                    result = System.getenv("DB_PATH");
+                }
+            }
+
+        } catch (Exception e) {
+            throw new RuntimeException("Error loading database configuration", e);
+        }
+        return result;
+    }
+
+    }
