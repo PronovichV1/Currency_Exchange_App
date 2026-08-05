b7d4be232cacfb454ca654d3de55a807090cd261
ValentinP1 <valikpronovic@gmail.com>
2026-08-05 13:25:20 +0200

    refactor: replace Currency model with CurrencyResponseDto in DTO response classes

--- src/main/java/com/currency/exchange/dto/response/ExchangeRateResponseDto.java
+++ src/main/java/com/currency/exchange/dto/response/ExchangeRateResponseDto.java
@@ -2,5 +2,5 @@ package com.currency.exchange.dto.response;
 
 import com.currency.exchange.model.Currency;
 
-public record ExchangeRateResponseDto(int id, Currency baseCurrency, Currency targetCurrency, double rate) {
+public record ExchangeRateResponseDto(int id, CurrencyResponseDto baseCurrency, CurrencyResponseDto targetCurrency, double rate) {
 }
